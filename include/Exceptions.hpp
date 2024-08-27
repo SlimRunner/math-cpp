2ac5b8e7f4f5de85d069e86803aa0b8c4ac1db9b
SlimRunner <dv.polygon@gmail.com>
2024-08-27 04:49:57 -0700

    Add more specific exceptions for parsing

--- include/Exceptions.hpp
+++ include/Exceptions.hpp
@@ -17,4 +17,28 @@ protected:
   std::size_t m_pos;
 };
 
+class invalid_symbol : public parse_error {
+  public:
+  invalid_symbol();
+  invalid_symbol(std::size_t at);
+  invalid_symbol(std::size_t at, char symbol);
+
+  virtual char const *what() const noexcept;
+  virtual char which() const noexcept;
+protected:
+  char m_sym;
+};
+
+class unexpected_symbol : public parse_error {
+  public:
+  unexpected_symbol();
+  unexpected_symbol(std::size_t at);
+  unexpected_symbol(std::size_t at, char symbol);
+
+  virtual char const *what() const noexcept;
+  virtual char which() const noexcept;
+protected:
+  char m_sym;
+};
+
 } // namespace err
--- src/Exceptions.cpp
+++ src/Exceptions.cpp
@@ -10,9 +10,36 @@ parse_error::parse_error() : m_pos{std::numeric_limits<std::size_t>::max()} {}
 parse_error::parse_error(std::size_t at) : m_pos{at} {}
 
 char const *parse_error::what() const noexcept {
-  return "Parse error occurred.";
+  return "Parse error occurred";
 }
 
 std::size_t parse_error::at() const noexcept { return m_pos; }
+
+invalid_symbol::invalid_symbol() : invalid_symbol::parse_error(), m_sym{'\0'} {}
+
+invalid_symbol::invalid_symbol(std::size_t at)
+    : invalid_symbol::parse_error(at), m_sym{'\0'} {}
+
+invalid_symbol::invalid_symbol(std::size_t at, char symbol)
+    : invalid_symbol::parse_error(at), m_sym{symbol} {}
+
+char const *invalid_symbol::what() const noexcept { return "Invalid symbol"; }
+
+char invalid_symbol::which() const noexcept { return m_sym; }
+
+unexpected_symbol::unexpected_symbol()
+    : unexpected_symbol::parse_error(), m_sym{'\0'} {}
+
+unexpected_symbol::unexpected_symbol(std::size_t at)
+    : unexpected_symbol::parse_error(at), m_sym{'\0'} {}
+
+unexpected_symbol::unexpected_symbol(std::size_t at, char symbol)
+    : unexpected_symbol::parse_error(at), m_sym{symbol} {}
+
+char const *unexpected_symbol::what() const noexcept {
+  return "Unexpected symbol encountered";
+}
+
+char unexpected_symbol::which() const noexcept { return m_sym; }
 
 } // namespace err
